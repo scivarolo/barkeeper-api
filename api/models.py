73ded7737335c5e3ae2cf82e48201e5a01115d14
Sebastian Civarolo <scivarolo@gmail.com>
2019-01-27 19:13:38 -0600

    setup basic login

--- api/models.py
+++ api/models.py
@@ -1,8 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class ApiUser(models.Model):
-    """Defines a user the same way used in json-server version of database."""
-    email = models.EmailField()
-    password = models.CharField('')
