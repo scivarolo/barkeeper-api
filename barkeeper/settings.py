c0c0b2e896ac1fa2edf07836ccfecffc4c32d70c
Sebastian Civarolo <scivarolo@gmail.com>
2019-02-01 23:07:14 -0600

    remove the pagination setting

--- barkeeper/settings.py
+++ barkeeper/settings.py
@@ -141,9 +141,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.IsAuthenticated',
     ],
-    'PAGE_SIZE': 10,
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.SearchFilter',
