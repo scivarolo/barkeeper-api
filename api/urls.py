02e778c3539723be66c6cfc9fed38faf4438d9b7
Sebastian Civarolo <scivarolo@gmail.com>
2019-02-17 00:21:22 -0600

    remove r in routes and unused imports

--- api/urls.py
+++ api/urls.py
@@ -1,20 +1,19 @@
 from django.conf.urls import include, url
-from django.contrib.auth.models import User
-from rest_framework import serializers, viewsets, routers
+from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
 
 from .models import *
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'cocktails', views.CocktailViewSet, 'cocktails')
-router.register(r'ingredients', views.IngredientViewSet, 'ingredients')
-router.register(r'cocktailingredients', views.CocktailIngredientViewSet, 'cocktailingredients')
-router.register(r'products', views.ProductViewSet, 'products')
-router.register(r'user_cocktails', views.UserCocktailViewSet, 'user_cocktails')
-router.register(r'user_tab', views.UserTabViewSet, 'user_tab')
-router.register(r'user_products', views.UserProductViewSet, 'user_products')
-router.register(r'user_shopping', views.UserShoppingViewSet, 'user_shopping')
+router.register('cocktails', views.CocktailViewSet, 'cocktails')
+router.register('ingredients', views.IngredientViewSet, 'ingredients')
+router.register('cocktailingredients', views.CocktailIngredientViewSet, 'cocktailingredients')
+router.register('products', views.ProductViewSet, 'products')
+router.register('user_cocktails', views.UserCocktailViewSet, 'user_cocktails')
+router.register('user_tab', views.UserTabViewSet, 'user_tab')
+router.register('user_products', views.UserProductViewSet, 'user_products')
+router.register('user_shopping', views.UserShoppingViewSet, 'user_shopping')
 
 urlpatterns = [
     url(r'^', include(router.urls)),
